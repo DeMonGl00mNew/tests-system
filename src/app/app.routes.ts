135c94a04b3431c86e97cbdb4d2d194ddd5a70a5
DeMonGl00m <demongl00m@yandex.ru>
2024-08-22 14:14:33 +0400

    добавление страницы configure в routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { TestViewverComponent } from './test-viewver/test-viewver.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
-
+import { ConfigureTestComponent } from './configure-test/configure-test.component';
 export const routes: Routes = [
     { path: '', component: HomePageComponent}, 
     { path: 'registration', component:RegistrationComponent },
     { path: 'test/:id', component:TestViewverComponent },
     { path: 'login', component:LoginComponent },
+    {path:'configure',component:ConfigureTestComponent},
     {path:'**',component:HomePageComponent}
 
 ];
--- src/app/configure-test/configure-test.component.ts
+++ src/app/configure-test/configure-test.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+interface Question {
+  text: string;
+  answers: string[];
+  correctAnswer: string;
+}
 @Component({
   selector: 'app-configure-test',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule], 
   templateUrl: './configure-test.component.html',
   styleUrl: './configure-test.component.scss'
-})
+})  
 export class ConfigureTestComponent {
-
+ 
 }
